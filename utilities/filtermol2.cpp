c710354d950936ed587e6757638b6b5fa65e45e4
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-01-25 16:40:00 +0800

    Updated filtermol2 to write the new *.mol2 to separate folders and the new ids to .csv

--- utilities/filtermol2.cpp
+++ utilities/filtermol2.cpp
@@ -5,12 +5,12 @@
 #include <algorithm>
 using namespace std;
 
-void output(const vector<string>& ids, const string& id, vector<string>& lines, ofstream& ofsnid)
+void output(const vector<string>& ids, const string& id, const string& slice, vector<string>& lines, ofstream& ofsnid)
 {
 	if (!binary_search(ids.cbegin(), ids.cend(), id))
 	{
 		ofsnid << id << '\n';
-		ofstream ofsmol2("mol2/" + id + ".mol2");
+		ofstream ofsmol2(slice + "/" + id + ".mol2");
 		for (const auto& l : lines)
 		{
 			ofsmol2 << l << '\n';
@@ -23,7 +23,7 @@ int main(int argc, char* argv[])
 {
 	if (argc != 5)
 	{
-		cout << "filtermol2 16_id.csv begin_slice end_slice 16_id_new.csv\n";
+		cout << "filtermol2 16_id.csv begin_slice end_slice\n";
 		return 0;
 	}
 
@@ -38,23 +38,23 @@ int main(int argc, char* argv[])
 	const size_t end = stoul(argv[3]);
 	vector<string> lines;
 	string id;
-	ofstream ofsnid(argv[4]);
 	for (size_t s = beg; s <= end; ++s)
 	{
 		const string slice = "16_p0." + to_string(s);
 		const string mol2 = slice + ".mol2";
 		cout << "Filtering " << slice << endl;
+		ofstream ofsnid(slice + ".csv");
 		for (ifstream ifs(mol2); getline(ifs, line); lines.push_back(line))
 		{
 			if (line == "@<TRIPOS>MOLECULE" && lines.size())
 			{
-				output(ids, id, lines, ofsnid);
+				output(ids, id, slice, lines, ofsnid);
 			}
 			else if (line[0] == 'Z')
 			{
 				id = line.substr(4); // id is 8 characters wide, without the ZINC prefix.
 			}
 		}
-		output(ids, id, lines, ofsnid);
+		output(ids, id, slice, lines, ofsnid);
 	}
 }
