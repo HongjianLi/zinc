07757d3c9b9f0afbccca2273e18f57f7c41b7b55
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-02-02 13:46:08 +0800

    Added summ

--- utilities/smmm.cpp
+++ utilities/smmm.cpp
@@ -5,7 +5,7 @@
 #include <algorithm>
 using namespace std;
 
-void output(const vector<double>& v)
+void output(const vector<size_t>& v)
 {
 }
 
@@ -16,11 +16,10 @@ int main(int argc, char* argv[])
 
 	// Determine the number of columns.
 	const size_t w = count(line.begin(), line.end(), ',') + 1;
-	vector<vector<double>> s(4);
+	vector<vector<size_t>> s(3);
 	s[0].resize(w, 0);
-	s[1].resize(w, numeric_limits<double>::max());
-	s[2].resize(w, numeric_limits<double>::lowest());
-	s[3].resize(w);
+	s[1].resize(w, numeric_limits<size_t>::max());
+	s[2].resize(w, numeric_limits<size_t>::min());
 	size_t n = 0;
 	do
 	{
@@ -28,7 +27,7 @@ int main(int argc, char* argv[])
 		{
 			c1 = line.find(',', c0);
 			if (c1 == string::npos) c1 = line.size();
-			const auto v = stod(line.substr(c0, c1 - c0));
+			const auto v = stoul(line.substr(c0, c1 - c0));
 			s[0][i] += v;
 			s[1][i] = min(s[1][i], v);
 			s[2][i] = max(s[2][i], v);
@@ -37,14 +36,13 @@ int main(int argc, char* argv[])
 
 	// Compute column-wise mean.
 	const auto q = 1.0 / n;
+	vector<double> m(w);
 	for (size_t i = 0; i < w; ++i)
 	{
-		s[3][i] = s[0][i] * q;
+		m[i] = s[0][i] * q;
 	}
 
 	// Output sum, min, max, mean
-	cout.setf(ios::fixed, ios::floatfield);
-	cout << setprecision(3);
 	for (const auto& v : s)
 	{
 		cout << v[0];
@@ -54,4 +52,12 @@ int main(int argc, char* argv[])
 		}
 		cout << endl;
 	}
+	cout.setf(ios::fixed, ios::floatfield);
+	cout << setprecision(3);
+	cout << m[0];
+	for (size_t i = 1; i < w; ++i)
+	{
+		cout << ',' << m[i];
+	}
+	cout << endl;
 }
