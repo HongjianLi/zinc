de7192331493f34cdfc5de8195180e5c87010814
Jacky Lee <JackyLeeHongJian@Gmail.com>
2015-06-22 14:50:03 +0800

    Updated encodehdr.cpp

--- utilities/encodehdr.cpp
+++ utilities/encodehdr.cpp
@@ -1,21 +1,12 @@
 #include <iostream>
-#include <fstream>
 #include <string>
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-	if (argc != 3)
-	{
-		cout << "encodeprop 16_hdr.tsv 16_hdr.bin\n";
-		return 0;
-	}
-
-	string line;
-	ofstream bin(argv[2]);
-	for (ifstream tsv(argv[1]); getline(tsv, line);)
+	for (string line; getline(cin, line);)
 	{
 		const size_t offset = stoul(line);
-		bin.write((char*)&offset, sizeof(offset));
+		cout.write(reinterpret_cast<const char*>(&offset), sizeof(offset));
 	}
 }
