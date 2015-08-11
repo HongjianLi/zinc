e37fc61e3ef42f16a6af6d0920fbaa31c4b5f78e
Jacky Lee <JackyLeeHongJian@Gmail.com>
2015-08-11 18:01:10 +0800

    Updated splitsdf.cpp to remove dependency on SDF keywords

--- utilities/splitsdf.cpp
+++ utilities/splitsdf.cpp
@@ -17,43 +17,28 @@ void dump(const vector<string>& lines, const string& filename)
 
 int main(int argc, char* argv[])
 {
-	if (argc != 2)
-	{
-		cout << "splitsdf all.sdf\n";
-		return 0;
-	}
-
 	const string delimiter = "$$$$";
-	const string drugbank_name_record = "> <DRUGBANK_ID>";
 	vector<string> lines;
 	lines.reserve(300);
-	string line, molecule_name;
-	for (ifstream in(argv[1]); getline(in, line);)
+	size_t id = 0;
+	for (string line; getline(cin, line);)
 	{
 		if (line == delimiter)
 		{
 			// Dump previous molecule.
-			dump(lines, molecule_name + ".sdf");
+			dump(lines, to_string(++id) + ".sdf");
 			lines.clear();
 		}
 		else
 		{
 			lines.push_back(line);
-			if (line == drugbank_name_record)
-			{
-				getline(in, line);
-				lines.push_back(line);
-				molecule_name = line;
-			}
 		}
 	}
 
 	// Dump the last molecule.
 	if (lines.size() > 2)
 	{
-		dump(lines, molecule_name + ".sdf");
+		dump(lines, to_string(++id) + ".sdf");
 	}
-
-	return 0;
 }
 
